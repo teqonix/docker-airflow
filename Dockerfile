91cc28d997b58e527b89896d222e0e0530b8f241
Travis Roesner <teqonix@gmail.com>
2020-05-30 20:44:45 -0600

    Continued work - db cloudformation works

--- Dockerfile
+++ Dockerfile
@@ -63,6 +63,7 @@ RUN set -ex \
     && pip install 'redis==3.2' \
     && pip uninstall -y sqlalchemy \
     && pip install sqlalchemy==1.3.15 \
+    && pip install password \
     && if [ -n "${PYTHON_DEPS}" ]; then pip install ${PYTHON_DEPS}; fi \
     && apt-get purge --auto-remove -yqq $buildDeps \
     && apt-get autoremove -yqq --purge \
--- teqonix_local_config/ec2_launch_script.sh
+++ teqonix_local_config/ec2_launch_script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+#Get AWs ECS client running:
+sudo yum update -y
+sudo amazon-linux-extras disable docker
+sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
+
+#Install git
 sudo yum --assumeyes install git
-sudo yum --assumeyes install docker
+
+# Will need to format the attached EBS volume on first-time create: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
+sudo mkfs -t xfs /dev/xvdb # How do we know what this is at runtime?
+sudo mkdir /usr/local/airflow
+sudo mount /dev/sdb /usr/local/airflow
